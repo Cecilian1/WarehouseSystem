15aa06edf196ee4accf4dd1aa8da9f5c2526f30e
Holmes3650 <1714602811@qq.com>
2026-08-28 01:01:59 +0800

    chore: ignore local env and runtime data

--- wechat-miniprogram/config/index.js
+++ wechat-miniprogram/config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   enableMock: false,
   enableDemoLogin: true,
-  baseUrl: 'http://10.28.32.201:8000/api',
-  wsUrl: 'ws://10.28.32.201:8000/ws/notify',
+  baseUrl: 'http://10.28.32.150:8000/api',
+  wsUrl: 'ws://10.28.32.150:8000/ws/notify',
   timeout: 10000,
   appName: '芯鲜管家',
   defaultDeviceId: 'fridge-01'
