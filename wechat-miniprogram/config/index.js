2a48d955c8f9982a9f2f93a2787030374656bc3b
Holmes3650 <1714602811@qq.com>
2026-08-27 13:03:44 +0800

    chore: update board address

--- wechat-miniprogram/config/index.js
+++ wechat-miniprogram/config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   enableMock: false,
   enableDemoLogin: true,
-  baseUrl: 'http://10.28.32.254:8000/api',
-  wsUrl: 'ws://10.28.32.254:8000/ws/notify',
+  baseUrl: 'http://10.28.32.252:8000/api',
+  wsUrl: 'ws://10.28.32.252:8000/ws/notify',
   timeout: 10000,
   appName: '芯鲜管家',
   defaultDeviceId: 'fridge-01'
